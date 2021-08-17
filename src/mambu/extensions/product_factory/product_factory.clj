86186a08b98fce73455c775fd94aaf7212e6bb11
mkersh <kershaw.mark@gmail.com>
2021-08-17 07:46:42 +0100

    update

---
+++ src/mambu/extensions/product_factory/product_factory.clj
@@ -0,0 +1,3 @@
+;;; TODO - Provide extensions to the standard Mambu product-factory APIs
+;;; Provide functions to support defining products via CasC
+(ns mambu.extensions.product-factory.product-factory)
