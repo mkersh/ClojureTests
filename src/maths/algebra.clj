d86e910230ccf8e3ab7d86802e87548c2d3f22cc
mkersh <kershaw.mark@gmail.com>
2021-09-26 15:14:28 +0100

    update

--- src/maths/algebra.clj
+++ src/maths/algebra.clj
@@ -6,17 +6,39 @@
 
 (ns maths.algebra)
 
+;;--------------------------------------------------------------------
+;; Term functions
+;; Terms are the parts of an Expression
+
+;; A term represents an atomic algebraic item
+;; So terms can be things like:
+;; Pure numbers: 3, 10, 15 etc
+;; These are represented in my notation as:
+;; 3 = term(3,[])
+;; 15 = term(15, [])
+;; Multiple of a pure number and variables:
+;; 3x, 5x^2, 6xy, etc
+;; These variables are represented by the var-list. So:
+;; 5x^2 = term(5,[:x :x])
+;; 6xy = term(6,[:x :y])
+;; NOTE: In my notation you need to use keywords as the variables
 (defn term [num var-list]
   (let [sorted-vlist (into [] (sort var-list))]
     {:term-num (bigdec num)
      :term-vlist sorted-vlist}))
 
+;; Multiple a term by mult1, where mult1 is either a number of a variable
 (defn term-multiple [term1 mult1]
   (let [num (:term-num term1)
         vlist (:term-vlist term1)]
       (if (keyword? mult1)
         (term num (conj vlist mult1))
         (term (* num mult1) vlist))))
+
+;;--------------------------------------------------------------------
+;; Expression functions
+;; An Expression consists of a number of Term parts
+;; Represented as a map keyed by (:term-vlist term)
 
 (defn add-to-expr [expr-map term1]
   (let [num (:term-num term1)
