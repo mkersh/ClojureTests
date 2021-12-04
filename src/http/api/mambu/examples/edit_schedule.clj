0ae4872f23be09d11f23624484a97d3159c7e76f
mkersh <kershaw.mark@gmail.com>
2021-12-04 20:25:46 +0000

    update

--- src/http/api/mambu/examples/edit_schedule.clj
+++ src/http/api/mambu/examples/edit_schedule.clj
@@ -9,15 +9,19 @@
   {:url (str "{{*env*}}/loans/" (:accid context) "/repayments")
    :method api/PATCH
    :query-params {}
-   :body {"Repayments" [
-    {"dueDate" "2020-02-01T01:00:00+01:00" 
-     "feesDue" 0
-     "feesUnappliedDue" 0
-     "interestDue" 5000.0
-     "isPaymentHoliday" false
-     "parentAccountKey" "8a818ff17d470d02017d4814072c19fe"
-     "penaltyDue" 0
-     "principalDue" 100}
+   :body 
+   
+   {"repayments" [
+    ;; {"dueDate" "2022-01-01"
+    ;;  "encodedKey" "8a818ff17d470d02017d4815600c1a5e"
+    ;;  "parentAccountKey" "8a818ff17d470d02017d4814072c19fe"
+    ;;  "principalDue" 90.00
+    ;;  }
+     {"dueDate" "2022-01-04"
+      "encodedKey" "8a818ef67d8012b4017d871724c14276"
+      "parentAccountKey" "8a818ef67d8012b4017d871446da41af"
+      "principalDue" 100.00
+      }
   ]}
    :headers {"Content-Type" "application/json"}
    
@@ -27,7 +31,7 @@
 (comment
 
 {;;"dueDate" "2022-01-01T01:00:00+01:00"
- "dueDate" "2022-01-01"
+ "dueDate" "2022-01-01" 
  "encodedKey" "8a818e3f7d56df38017d5801dc552afc"
  "parentAccountKey" "8a818e3f7d56df38017d57ff74dd2a77"}
 
@@ -37,7 +41,7 @@
  "encodedKey" "8a818e3f7d56df38017d5801dc462aee"
  "feesDue" 0
  "feesUnappliedDue" 0
- "interestDue" 5000.0
+ "interestDue" 24.54
  "isPaymentHoliday" false
  "parentAccountKey" "8a818e3f7d56df38017d57ff74dd2a77"
  "penaltyDue" 0
@@ -66,7 +70,7 @@
 
 (comment
   (api/setenv "env2")
-  (def accid "XVKB253")
+  (def accid "NMMZ161")
   
   (api/PRINT (:last-call (steps/apply-api edit-loan-schedule {:accid accid})))
   (api/PRINT (:last-call (steps/apply-api get-loan-schedule {:accid accid})))
