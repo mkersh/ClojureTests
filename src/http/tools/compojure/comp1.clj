3939f4fc9a97303e7d19c2bb41d04f07c06373e7
mkersh <kershaw.mark@gmail.com>
2022-01-25 17:40:35 +0000

    update

--- src/http/tools/compojure/comp1.clj
+++ src/http/tools/compojure/comp1.clj
@@ -6,13 +6,16 @@
             [ring.adapter.jetty :as jet]
             [ring.middleware.reload :as reload]
             [ring.middleware.params :as wrap]
+            [ring.middleware.resource :as res]
+            [ring.util.response :as resp]
             
             ))
 
 (defroutes app
-  (GET "/" [] "<h1>Hello World!!!</h1>")
+  (GET "/" [] "<h1>Hello World!!</h1>")
+  (GET "/public" [] (resp/resource-response "public/test.txt"))
   (GET "/about" request (str "<h1>Hello World!!!</h1>" request))
-  (GET "/api/get-all-customers" [] "<h1>These are all the customers</h1>")
+  (GET "/api/get-all-customers" [] "<h1>Return all the customers</h1>")
   (GET "/api/get-customer/:id" [id] (str "<h1>Getting Customer " id "</h1>"))
   ;; The following inline regexpressions don't work for me!! {[0-9]+}
   (GET "/api/get-customer2/:id" [id] (str "<h1>Getting Customer " id "</h1>"))
@@ -22,7 +25,7 @@
 ;; https://github.com/ring-clojure/ring/issues/104
 (def app-with-reload
   ;; Using two middleware handlers here
-  (wrap/wrap-params (reload/wrap-reload #'app)))
+  (res/wrap-resource (wrap/wrap-params (reload/wrap-reload #'app)) "public"))
 
 (defonce server (jet/run-jetty #'app-with-reload {:join? false :port 3000}))
 
@@ -30,7 +33,9 @@
 ;; https://ring-clojure.github.io/ring/ring.adapter.jetty.html
 
 ;;(jet/run-jetty app {:join? false :port 3000})
-(.start server)
+(.start server) ;; http://localhost:3000
 (.stop server)
+
+(resp/resource-response "public/test.txt")
 ;;
 )
