38cacd1bcb5236486eb065df4c6a80c4b24f222c
mkersh <kershaw.mark@gmail.com>
2022-01-30 13:13:02 +0000

    update

--- src/namespaces/project_extraction.clj
+++ src/namespaces/project_extraction.clj
@@ -94,7 +94,7 @@
 
 (comment
   ;; [1] Function for creating a mini-project from a given namespace
-  (create-new-project "/Users/mkersh/clojure/Shared/example-220130/" "/Users/mkersh/clojure/ClojureTests/" "http.api.mambu.examples.edit_schedule")
+  (create-new-project "/Users/mkersh/clojure/Shared/example-220130b/" "/Users/mkersh/clojure/ClojureTests/" "http.api.mambu.examples.edit_schedule")
 
   ;; Testing stuff whilst developing this library
   (delete-dir "/Users/mkersh/clojure/Shared/NewProj/")
@@ -103,7 +103,7 @@
   (re-seq #"\[.*\]" "[clojure.string :as str] \n [clojure.java.io :as io]")
 
 
-;;
+;; 
 (sh/sh "ls" "-aul")
 (sh/sh "ln" "/Users/mkersh/clojure/ClojureTests/src/http/ENV.clj" "/Users/mkersh/clojure/Shared/NewProj/src/http/ENV.clj")
 
