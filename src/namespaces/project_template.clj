51acd4bb00e6ba0c0cad7c01c87f48f4e426a07a
mkersh <kershaw.mark@gmail.com>
2022-01-30 20:04:34 +0000

    update

--- src/namespaces/project_template.clj
+++ src/namespaces/project_template.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/MkershMambu"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.0.0" 
   :uberjar-name "mk-mini-proj-standalone.jar"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "1.3.610"]
