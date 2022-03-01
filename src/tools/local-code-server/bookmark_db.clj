ebdc8cd917dd6634a509af5527ae1797b7dbffac
mkersh <kershaw.mark@gmail.com>
2022-03-01 15:50:08 +0000

    update

---
+++ src/tools/local-code-server/bookmark_db.clj
@@ -0,0 +1,110 @@
+;;; Bookmark functions:
+;;; To support GotoFile working from a bookmark only
+;;;
+;;; Requires:
+;;; - (1) Keeping an in-memory cache of known bookmark
+;;; - (2) Persisting #1 to disk
+;;; - (3) Being able to refresh #1 with new added bookmarks
+;;;
+;;; Bookmark's have the following format: #bookmark= 9d814ac2-02dc-47a5-980d-3a23108de57a
+;;; The plan is to place these in clojure src files and for the GotoFile tool to be able to jump to these
+;;;
+(ns tools.local-code-server.bookmark-db
+  (:require  [clojure.java.io :as io]
+             [clojure.java.shell :as sh]
+             [clojure.pprint :as pp]
+             [clojure.string :as str]))
+
+(defonce BM-ROOT (atom "BOOKMARKS/"))
+(defonce BOOKMARK_CACHE (atom {}))
+
+;; *************************************
+;; Functions for reading/writing the @BOOKMARK_CACHE to disk
+;;
+
+(defn delete-directory-recursive
+  "Recursively delete a directory."
+  [^java.io.File file]
+  ;; when `file` is a directory, list its entries and call this
+  ;; function with each entry. can't `recur` here as it's not a tail
+  ;; position, sadly. could cause a stack overflow for many entries?
+  (when (.isDirectory file)
+    (doseq [file-in-dir (.listFiles file)]
+      (delete-directory-recursive file-in-dir)))
+  ;; delete the file or directory. if it it's a file, it's easily
+  ;; deletable. if it's a directory, we already have deleted all its
+  ;; contents with the code above (remember?)
+  (io/delete-file file))
+
+(defn delete-BM []
+  (try (reset! BOOKMARK_CACHE {})
+       (delete-directory-recursive (clojure.java.io/file @BM-ROOT))
+       (catch Exception _ "Nothing to DELETE")))
+
+(defn save-to-file
+  [file-name s]
+  (spit file-name s))
+
+;; Get the string format to save to the file
+;; NOTE: Planning to save as Clojure EDN format (at least initially)
+(defn get-object-str [object]
+  (let [out (java.io.StringWriter.)]
+    (pp/pprint object out)
+    (.toString out)))
+
+;; Save object to a file
+(defn save-bookmarks [object]
+  (let [file-path (str @BM-ROOT "bookmarks.edn")
+        object-str (get-object-str object)]
+    ;;(prn "save-object:" file-path)    
+    (io/make-parents file-path)
+    (save-to-file file-path object-str)))
+
+(defn read-object [fpath]
+  (read-string (slurp fpath)))
+
+(defn read-bookmarks []
+  (let [fpath (str @BM-ROOT "bookmarks.edn")]
+   (reset! BOOKMARK_CACHE (read-object fpath))))
+
+
+;; *************************************
+;; Main Bookmark functions
+;;
+
+(defn add-bookmark [bookmark-uuid bookmark-obj]
+  (reset! BOOKMARK_CACHE (assoc @BOOKMARK_CACHE bookmark-uuid bookmark-obj)))
+
+
+;; https://rosettacode.org/wiki/Walk_a_directory/Recursively#Clojure
+(defn walk-dir [dirpath pattern]
+  (doall (filter #(re-matches pattern (.getName %))
+                 (file-seq (io/file dirpath)))))
+
+(defn process-file [fn]
+(prn "Processing File" fn)
+)
+
+(defn parse-find-bookmarks [placestolook-list _ignore-list]
+  (loop [dir-list placestolook-list]
+    (let [dir1 (first dir-list)
+          rest-list (next placestolook-list)]
+      (doall (map #(process-file (.getPath %)) (walk-dir dir1 #".*\..*")))
+      (when rest-list (recur rest-list)))))
+
+(comment
+
+(parse-find-bookmarks ["src/tools/local-code-server"] "")
+
+(re-matches #".*\.edn" "hhh.edn")
+(save-bookmarks {:f1 :v1 :f2 [2 3 4 5 6 7]})
+(save-bookmarks @BOOKMARK_CACHE)
+(read-bookmarks)
+(delete-BM)
+(add-bookmark "sgsgsgs" {})
+(add-bookmark "sgsgsgs2" {})
+(add-bookmark "sgsgsgs3" {})
+
+@BOOKMARK_CACHE
+;;
+)
