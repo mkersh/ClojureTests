93a8635f23f6fd1bbeeedbf264e4e6c956895db0
mkersh <kershaw.mark@gmail.com>
2022-06-26 11:08:03 +0100

    update

--- src/project_euler/support.clj
+++ src/project_euler/support.clj
@@ -4,6 +4,32 @@
 ;;; see https://stackoverflow.com/questions/42700407/immediately-kill-a-running-future-thread
 (ns project-euler.support)
 
+;;; ***************************************************
+;;; Code to redirect all output to the REPL
+;;;
+;; https://stackoverflow.com/questions/34576109/configure-clojure-logging-to-output-to-nrepl#38294275
+;; run this code on the repl where you wish to see all output.
+;; You will need to add the dependency [commons-io "2.4"] to your
+;; leiningen dependencies.
+(import 'org.apache.commons.io.output.WriterOutputStream)
+(import 'java.io.PrintStream)
+
+(defn setup-output-to-repl []
+  ;; First, we redirect the raw stdout of the server to this repl
+  (System/setOut (PrintStream. (WriterOutputStream. *out*)
+                               true)) ;; Auto-flush the PrintStream
+
+  ;; Next, we alter the root binding of *out* so that new threads
+  ;; send their output to THIS repl rather than the original System/out.
+  (alter-var-root #'*out* (fn [_] *out*))
+
+  ;; Now the snippets should both send output to this repl:
+  (.println System/out "Hello stdout.")
+  (.start (Thread. #(println "Hello from a new thread."))))
+
+(defonce _setupoutput (setup-output-to-repl))
+_setupoutput
+
 (defonce JOB-LIST (atom {}))
 
 (defn start-task [name f args]
@@ -61,3 +87,4 @@
   (kill-task :job1)
 ;;
   )
+
